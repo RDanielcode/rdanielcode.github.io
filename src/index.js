1f3829638e3048553590ac2e8e666ce4a370fb38
Danielcode <danielalejndro@gmail.com>
2022-06-04 11:59:35 -0400

    downgrade to react 17

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
+// import { createRoot } from 'react-dom/client'
 import { App } from './components/App'
 import { Provider } from './components/Context/Context'
 import './index.css'
 
-createRoot(document.getElementById('app')).render(
+// createRoot(document.getElementById('app')).render(
+//   <Provider>
+//     <App />
+//   </Provider>
+// )
+
+ReactDOM.render(
   <Provider>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById('app')
 )
